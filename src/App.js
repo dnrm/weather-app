300655ff5516acff08097a7efac0c8dd6a7ea0e0
Daniel Medina <daniel@medina.com>
2021-05-07 11:39:35 -0500

    Removed unused useEffect call

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Results from "./components/Results";
 
 function App() {
     const [city, setCity] = useState("London");
-    // eslint-disable-next-line no-unused-vars
     const [unit, setUnit] = useState("metric");
     const [weather, setWeather] = useState("");
     const [temperature, setTemperature] = useState("");
@@ -32,8 +31,6 @@ function App() {
 
         }
     }, [city, unit]);
-
-    useEffect(() => {}, [weather]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
