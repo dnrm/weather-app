3099d7ea4ecb50a0e35a4ecd0f8a6582357ece42
Daniel Medina <daniel@medina.com>
2021-06-12 19:13:23 -0500

    :sparkles: Introduced a ton of new features
    1. Added icon to search button
    2. Added load animation with AnimeJS
    3. Added search animation with AnimeJS
    4. Added my name as a footer
    5. Removed autocomplete and spell check in search input
    6. Changed title from 'Weather App' to 'Weather'

--- src/App.js
+++ src/App.js
@@ -2,37 +2,87 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import Form from "./components/Form";
 import Results from "./components/Results";
+import anime from "animejs";
 
 function App() {
     const [city, setCity] = useState("London");
+    // eslint-disable-next-line
     const [unit, setUnit] = useState("metric");
     const [weather, setWeather] = useState("");
     const [temperature, setTemperature] = useState("");
     const API = "c73eea09d5030ffeaafaed52a3fafa78";
 
     useEffect(() => {
+        document.addEventListener('keydown', () => {
+            document.getElementById('input').focus();
+        })
+
+        return () => {
+            document.removeEventListener('keydown', () => {
+                document.getElementById('input').focus();
+            })
+        }
+    }, [])
+
+    useEffect(() => {
+        anime({
+            targets: ["#cover"],
+            width: 0,
+            duration: 100,
+            easing: 'easeInOutQuad'
+        });
+
+        anime({
+            targets: ['.form'],
+            duration: 2000,
+            keyframes: [
+                { opacity: 0 },
+                { opacity: 1 }
+            ]
+        })
+
+        anime({
+            targets: ['.results'],
+            duration: 3000,
+            keyframes: [
+                { opacity: 0 },
+                { opacity: 1 }
+            ]
+        })
+    }, []);
+
+    useEffect(() => {
         try {
             fetch(
                 `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API}&units=${unit}`
-            ).then((i) => {
-                if (i.status === 200) {
-                  return i.json()
-                } else {
-                  return false;
-                }
-            })
-            .then(i => {
-              if (i.main && i.weather) {
-                setTemperature(Math.ceil(i.main.temp));
-                setWeather(i.weather[0].main);
-              }
-            });
-        } catch (e) {
-
-        }
+            )
+                .then((i) => {
+                    if (i.status === 200) {
+                        return i.json();
+                    } else {
+                        return false;
+                    }
+                })
+                .then((i) => {
+                    console.log(i)
+                    if (i.main && i.weather) {
+                        setTemperature(Math.ceil(i.main.temp));
+                        setWeather(i.weather[0].main);
+                    }
+                });
+        } catch (e) {}
     }, [city, unit]);
 
     const handleSubmit = (e) => {
+        anime({
+            targets: ['.results'],
+            duration: 1000,
+            keyframes: [
+                { opacity: 0 },
+                { opacity: 1 }
+            ],
+            easing: 'linear'
+        })
         e.preventDefault();
         setCity(
             e.target[0].value.replace(/\w\S*/g, (w) =>
@@ -42,15 +92,18 @@ function App() {
     };
 
     return (
-        <main>
-            <Form onSubmit={handleSubmit} />
-            <Results
-                weather={weather}
-                temperature={temperature}
-                unit={unit}
-                city={city}
-            />
-        </main>
+        <>
+            <div id="cover"></div>
+            <main>
+                <Form onSubmit={handleSubmit} />
+                <Results
+                    weather={weather}
+                    temperature={temperature}
+                    unit={unit}
+                    city={city}
+                />
+            </main>
+        </>
     );
 }
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,74 @@
-import React from 'react'
-import '../styles/form.css';
+import React, { useRef, useState } from "react";
+import "../styles/form.css";
 
 export default function Form(props) {
+    const buttonElement = useRef();
+
+    const [hovered, setHovered] = useState(false);
+
+    const handleMouseOver = () => setHovered(true);
+    const handleMouseLeave = () => setHovered(false);
+
     return (
         <div className="form">
-            <h1>Weather App</h1>
+            <h1>Weather</h1>
             <form onSubmit={props.onSubmit}>
                 <p>City Name</p>
-                <input type="text" placeholder="City"/>
-                <button type="submit">Go</button>
+                <input
+                    ref={buttonElement}
+                    type="text"
+                    placeholder="City"
+                    id="input"
+                    autoComplete="off"
+                    spellCheck="false"
+                />
+                <button
+                    id="search"
+                    type="submit"
+                    onMouseOver={handleMouseOver}
+                    onMouseLeave={handleMouseLeave}
+                >
+                    {!hovered ? (
+                        <>
+                            <p>Search</p>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="icon-search icon"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth={2}
+                                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                                />
+                            </svg>
+                        </>
+                    ) : (
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="icon-search icon"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                            />
+                        </svg>
+                    )}
+                </button>
             </form>
+            <footer className="footer">
+                <p>
+                    © <a href="https://github.com/dnrm">Daniel Medina</a> 2021
+                </p>
+            </footer>
         </div>
-    )
+    );
 }
